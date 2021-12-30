8a58659f29d104c86036a874640229109764d044
Eduard Hovhannisyan <eduardhovhannisyan@Eduards-MacBook-Pro.local>
2021-12-30 00:20:58 -0800

    submitting semi final project

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Directory from "./comps/Directory";
 import SecondBanner from "./comps/SecondBanner";
 import Services from "./comps/Services";
 import TopBanner from "./comps/TopBanner";
+import WebTitle from "./comps/WebTitle";
 
 function App() {
   return (
     <div className="app_container">
+      <WebTitle />
       <CallButton />
       <TopBanner />
       <SecondBanner />
---
+++ src/comps/WebTitle.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./WebTitle.css";
+
+function WebTitle() {
+  
+  return (
+    <div className="web_title_container">
+      <h1>Brand Home Health Care, Inc.</h1>
+    </div>
+  );
+}
+
+export default WebTitle;

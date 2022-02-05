5867103b7624265e07065f4e7b8bb47bd4d9d987
Eduard Hovhannisyan <eduardhovhannisyan@Eduards-MacBook-Pro.local>
2022-02-04 17:54:24 -0800

    changing SEO changes - 1

--- src/comps/WebTitle.js
+++ src/comps/WebTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./WebTitle.css";
 
 function WebTitle() {
-  
+
   return (
     <div className="web_title_container">
       <h1>Brand Home Health Care, Inc.</h1>
